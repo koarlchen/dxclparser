672f1ee3fd04baa9f3a3fe64b1bc596beb5b474e
max <max@karl.wf>
2022-05-15 16:46:27 +0200

    Add file parser example

---
+++ examples/file.rs
@@ -0,0 +1,30 @@
+extern crate dxclparser;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Invalid number of arguments.");
+        eprintln!("Usage: {} <file>", args[0]);
+    } else {
+        let filepath = Path::new(&args[1]);
+        let file = File::open(filepath).expect("Failed to open file");
+        let reader = io::BufReader::new(file).lines();
+
+        for line in reader {
+            match dxclparser::parse(line.unwrap().trim().trim_end_matches('\u{0007}')) {
+                Ok(spot) => {
+                    println!("{}", spot.to_json());
+                }
+                Err(e) => {
+                    eprintln!("Failed to parse spot ({})", e);
+                }
+            }
+        }
+    }
+}
