3c1a937b82f25bd34357b5ed5a1115f9c7c357c9
max <max@karl.wf>
2022-05-15 21:55:35 +0200

    Fix basic example script

--- examples/basic_run.sh
+++ examples/basic_run.sh
@@ -4,6 +4,6 @@ call="YOUR_CALL"
 host="SERVER_HOST"
 port=SERVER_PORT
 
-cat <(echo "$call") - | nc $host $port 2>&1 | xargs -I{} ./parser_basic {}
+cat <(echo "$call") - | nc $host $port 2>&1 | xargs -I{} ./basic {}
 
 exit 0
